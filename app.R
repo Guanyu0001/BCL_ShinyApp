5849659f41f3a341f2a102ba2ff7c51ffb5884bb
Guanyu0001 <chenguanyu.ubc@gmail.com>
2021-11-25 21:52:04 -0800

    Update app.R

--- app.R
+++ app.R
@@ -1,64 +1,88 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(tidyverse)
+library(DT)
 bcl <- read.csv("bcl-data.csv")
-options(shiny.autoreload=T)
+options(shiny.autoreload = T)
 
-
-# define areas for output. note that order matters.
 ui <- fluidPage(
-    titlePanel("BCL app"),
-    "Expanation goes here.",
-    tags$br(),
-    tags$br(),
-    sidebarLayout(
-        sidebarPanel(
-          sliderInput("my_slider","Select a price range", max = 200, min= 0, value = c(10,30)),
-          radioButtons("my_radio","Select beverage type:" ,
-               choices = unique(bcl$Type))
-                ),
+  titlePanel("BCL app"),
+  "This is a interactive app to help user select beverages they like by types and prices. The results can be downloaded.",
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("my_slider", "Select a price range", max = 200, min = 0, value = c(10, 30)), # the slider for selecting the price
 
-        mainPanel(
-          plotOutput("my_plot"),
-          tableOutput("my_table"))
-    )
+      checkboxGroupInput("my_radio", "Select beverage type:",
+        choices = unique(bcl$Type),
+        selected = unique(bcl$Type)
+      ), # checkbox for selecting the types of beverages
 
+      checkboxInput("dscending", "Dscending by price", FALSE), # checkbox for soring the results
+
+      verbatimTextOutput("value"), # for checking the output of checkboxInput
+
+      textOutput("options"), # for providing the total number of options
+
+      downloadButton("downloadData", "Download") # for downloading
+    ),
+    mainPanel(
+      plotOutput("my_plot"), # for the histogram
+      DT::dataTableOutput("my_table")
+    ) # for the summary table
+  ),
 )
 
 # do the analysis for the output
 server <- function(input, output) {
-
   filtered <- reactive({
-    # print(input$my_slider)# use a curly bracket to input two arguments into reactive() which required only one input.
-    # print(input$my_radio)
-    bcl %>%
-    filter(Price<=input$my_slider[2],
-         Price>=input$my_slider[1],
-         Type == input$my_radio)
+    # if-else is used for descending the results by user's choice
+    if (input$dscending) {
+      bcl %>%
+        filter(
+          Price <= input$my_slider[2],
+          Price >= input$my_slider[1],
+          Type %in% input$my_radio
+        ) %>%
+        arrange(desc(Price))
+    } else {
+      bcl %>%
+        filter(
+          Price <= input$my_slider[2],
+          Price >= input$my_slider[1],
+          Type %in% input$my_radio
+        ) %>%
+        arrange(Price)
     }
+  })
+
+  # get the histogram
+  output$my_plot <- renderPlot(
+    filtered() %>%
+      ggplot(aes(Alcohol_Content)) +
+      geom_histogram()
   )
 
-   output$my_plot <- renderPlot(
-     filtered() %>%
-           ggplot(aes(Alcohol_Content))+
-               geom_histogram()
-   )
+  # get the table
+  output$my_table <- DT::renderDataTable(filtered())
 
-  output$my_table <- renderTable(
-  filtered()
-    )
+  output$value <- renderText(
+    if (input$dscending) {
+      print("The order is dscending by price.")
+    } else {
+      print("The order is ascending by price.")
+    }
+  ) # for checking the output of checkboxInput
 
+  output$options <- renderText(paste("We found ", nrow(filtered()), " options for you.")) # providing the number of options
 
-
-
+  # settings for download
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste("BCL_selected_", Sys.Date(), ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(filtered(), file)
+    }
+  )
 }
 
 # Run the application
