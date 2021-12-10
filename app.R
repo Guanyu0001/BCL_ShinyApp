6eb0a7dff7c899b081c786e51f83bbbf08b89028
Guanyu0001 <chenguanyu.ubc@gmail.com>
2021-12-10 11:26:41 -0800

    Update app.R

--- app.R
+++ app.R
@@ -6,28 +6,43 @@ options(shiny.autoreload = T)
 
 ui <- fluidPage(
   titlePanel("BCL app"),
-  "This is a interactive app to help user select beverages they like by types and prices. The results can be downloaded.",
+  "This is a interactive app to help users select beverages they like by types and prices. There are three functions can help reader select the beverages: 
+  1. select the price range, 2. select beverage type, and 3. Sort the results. Textboxes can help users check inputs. The filtered records are visualized by a histgram and a table.
+  The filtered records can be downloaded. Thank your for trying this app. Feel free to let me know if you have any questions.",
+  br(),
   sidebarLayout(
     sidebarPanel(
-      sliderInput("my_slider", "Select a price range", max = 200, min = 0, value = c(10, 30)), # the slider for selecting the price
+      sliderInput("my_slider", "Select a Price Range:", max = 200, min = 0, value = c(10, 30)), # the slider for selecting the price
+      verbatimTextOutput("price_range_selected"),
+      br(),
 
-      checkboxGroupInput("my_radio", "Select beverage type:",
+      # checkbox for selecting the types of beverages
+      checkboxGroupInput("my_radio", "Select Beverage Type:",
         choices = unique(bcl$Type),
         selected = unique(bcl$Type)
-      ), # checkbox for selecting the types of beverages
+      ),
+      verbatimTextOutput("beverage_type_selected"),
+      br(),
 
-      checkboxInput("dscending", "Dscending by price", FALSE), # checkbox for soring the results
+      # checkbox for sorting the results
+      strong("Order of Results:"),
+      checkboxInput("descending", "Descending by price", FALSE),
+      verbatimTextOutput("value"),
+      br(),
 
-      verbatimTextOutput("value"), # for checking the output of checkboxInput
+      # for providing the total number of options
+      strong("Filtered Results:"),
+      verbatimTextOutput("options"),
+      br(),
 
-      textOutput("options"), # for providing the total number of options
-
-      downloadButton("downloadData", "Download") # for downloading
+      # for downloading
+      strong("Download Filtered Results:"),
+      downloadButton("downloadData", "Download")
     ),
     mainPanel(
       plotOutput("my_plot"), # for the histogram
-      DT::dataTableOutput("my_table")
-    ) # for the summary table
+      DT::dataTableOutput("my_table") # for the summary table
+    )
   ),
 )
 
@@ -35,7 +50,7 @@ ui <- fluidPage(
 server <- function(input, output) {
   filtered <- reactive({
     # if-else is used for descending the results by user's choice
-    if (input$dscending) {
+    if (input$descending) {
       bcl %>%
         filter(
           Price <= input$my_slider[2],
@@ -65,14 +80,20 @@ server <- function(input, output) {
   output$my_table <- DT::renderDataTable(filtered())
 
   output$value <- renderText(
-    if (input$dscending) {
-      print("The order is dscending by price.")
+    if (input$descending) {
+      paste("The order is descending by price.")
     } else {
-      print("The order is ascending by price.")
+      paste("The order is ascending by price.")
     }
-  ) # for checking the output of checkboxInput
+  )
 
-  output$options <- renderText(paste("We found ", nrow(filtered()), " options for you.")) # providing the number of options
+  # for demonstrating the inputs:
+  # display the selected price range for beverages
+  output$price_range_selected <- renderText(paste("The beverages from", input$my_slider[1],"CAD", "to", input$my_slider[2], "CAD were selected."))
+  # display the selected types of beverages
+  output$beverage_type_selected <- renderText(paste(paste(input$my_radio, collapse = " "), "were selected."))
+  # providing the number of options
+  output$options <- renderText(paste("We found ", nrow(filtered()), " options for you."))
 
   # settings for download
   output$downloadData <- downloadHandler(
